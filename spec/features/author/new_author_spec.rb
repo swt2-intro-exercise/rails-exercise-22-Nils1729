99c2bdcac50d1f9a866c5d842f9d4d99fe76bc0d
Nils Hanff <Nils.Hanff@student.hpi.de>
2022-10-28 18:53:32 +0200

    Resolve #4

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -19,6 +19,6 @@ describe "New author page", type: :feature do
     page.fill_in 'author[last_name]', with: 'Turing'
     page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
 
-    find(input[type="submit"]).click
+    find('input[type="submit"]').click
   end
 end
