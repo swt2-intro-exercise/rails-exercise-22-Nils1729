b22cfb80b421a31861d1a3aabd29c1fdd5674cda
Nils Hanff <Nils.Hanff@student.hpi.de>
2022-10-29 17:32:58 +0200

    Failing test for #6

---
+++ spec/features/author/index_author_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Index author page", type: :feature do
+    before(:each) do
+        @alan = FactoryBot.create :author
+    end
+
+    it "should exist at 'index_author_path' and render withour error" do
+        visit authors_path
+    end
+
+    it "should display the author's details" do
+        visit authors_path
+        expect(page).to have_text "Alan Turing"
+        expect(page).to have_text "http://wikipedia.de/Alan_Turing"
+    end
+
+    it "should link to authors detail pages" do
+        visit authors_path
+        expect(page).to have_link "Details", href: author_path(@alan)
+    end
+
+    it "should link to 'new_author_path'" do
+        visit authors_path
+        expect(page).to have_link "New", href: new_author_path
+    end
+end
