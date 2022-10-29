41a82c4ff54cd77566c70b1a5eec3ad0bab0752c
Nils Hanff <Nils.Hanff@student.hpi.de>
2022-10-29 22:03:39 +0200

    Closes #15

--- spec/features/paper/index_paper_spec.rb
+++ spec/features/paper/index_paper_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe "Index paper page", type: :feature do
     before(:each) do
         @example_paper = Paper.new(title: "Examples", venue: "latest newspapers", year: 1970)
+        @example_paper.save
     end
 
     it "should link to paper edit pages" do
