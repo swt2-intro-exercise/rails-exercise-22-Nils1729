53442c2a73ec4fcddd6bb2fafd05802dff72391e
Nils Hanff <Nils.Hanff@student.hpi.de>
2022-10-29 20:32:16 +0200

    Closes #10

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -20,6 +20,16 @@ class AuthorsController < ApplicationController
     def edit
         @author = Author.find params[:id]
     end
+
+    def update
+        @author = Author.find params[:id]
+
+        if @author.update(author_params)
+            redirect_to root_path, notice: 'Success!'
+        else
+            render 'edit'
+        end
+    end
     
     def index
         @authors = Author.all
