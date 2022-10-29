27ded89bc3cb7d3e7acfd13527ea3cf1a8904a62
Nils Hanff <Nils.Hanff@student.hpi.de>
2022-10-29 20:25:23 +0200

    Failing test for #10

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -8,4 +8,15 @@ describe "Edit author page", type: :feature do
     it "should exist at 'edit_author_path' and render withour error" do
         visit edit_author_path(@alan)
     end
+
+    it "should update the author details" do
+        visit edit_author_path(@alan)
+
+        page.fill_in 'author[first_name]', with: 'Bob'
+        find('input[type="submit"]').click
+
+        @alan.reload
+
+        expect(@alan.first_name).to eq 'Bob'
+    end
 end
